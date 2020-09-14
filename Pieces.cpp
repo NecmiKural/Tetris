2878566f083e0b0c955bec1529236db1a0a2e961
anilberg <anilberg@users.noreply.github.com>
2020-09-14 22:59:17 +0300

    Update Pieces.cpp

--- Pieces.cpp
+++ Pieces.cpp
@@ -283,22 +283,6 @@ int PiecesInitialPosition  [7 /*kind */ ][4 /* r2otation */ ][2 /* position */] 
         {-1, -3}    //4th rotation
     },
 
-    //L
-    {
-        {-2, -2},   //1st rotation
-        {-2, -2},   //2nd rotation
-        {-3, -2},   //3rd rotation
-        {-4, -2}    //4th rotation
-    },
-
-    //L mirrored
-    {
-        {-2, -2},   //1st rotation
-        {-2, -2},   //2ndrotation
-        {-4, -2},   //3rd rotation
-        {-2, -2}    //4th rotation
-    },
-     
     //N
     {
         {-2, -3},   //1st rotation
@@ -313,5 +297,23 @@ int PiecesInitialPosition  [7 /*kind */ ][4 /* r2otation */ ][2 /* position */] 
         {-2, -3},   //2nd rotation
         {-2, -3},   //3rd rotation
         {-2, -2}    //4th rotation
+    },
+
+    //L
+    {
+        {-2, -2},   //1st rotation
+        {-2, -2},   //2nd rotation
+        {-3, -2},   //3rd rotation
+        {-4, -2}    //4th rotation
+    },
+
+    //L mirrored
+    {
+        {-2, -2},   //1st rotation
+        {-2, -2},   //2ndrotation
+        {-4, -2},   //3rd rotation
+        {-2, -2}    //4th rotation
     }
+     
+
 };
