d4ddce4bebf0f0d49148f7c8b63ef14615636657
anilberg <anilberg@users.noreply.github.com>
2020-09-14 23:20:09 +0300

    Update Pieces.cpp

--- Pieces.cpp
+++ Pieces.cpp
@@ -1,3 +1,57 @@
+#include "Pieces.h"
+
+/* 
+======================================                                  
+Return the type of a block (0 = no-block, 1 = normal block, 2 = pivot block)
+ 
+Parameters:
+ 
+>> pPiece:        Piece to draw
+>> pRotation: 1 of the 4 possible rotations
+>> pX:            Horizontal position in blocks
+>> pY:            Vertical position in blocks
+====================================== 
+*/
+int Pieces::GetBlockType (int pPiece, int pRotation, int pX, int pY)
+{
+    return PiecesArray [pPiece][pRotation][pX][pY];
+}
+ 
+ 
+/* 
+======================================                                  
+Returns the horizontal displacement of the piece that has to be applied in order to create it in the
+correct position.
+ 
+Parameters:
+ 
+>> pPiece:    Piece to draw
+>> pRotation: 1 of the 4 possible rotations
+====================================== 
+*/
+int Pieces::GetXInitialPosition (int pPiece, int pRotation)
+{
+    return PiecesInitialPosition [pPiece][pRotation][0];
+}
+ 
+ 
+/* 
+======================================                                  
+Returns the vertical displacement of the piece that has to be applied in order to create it in the
+correct position.
+ 
+Parameters:
+ 
+>> pPiece:    Piece to draw
+>> pRotation: 1 of the 4 possible rotations
+====================================== 
+*/
+int Pieces::GetYInitialPosition (int pPiece, int pRotation)
+{
+    return PiecesInitialPosition [pPiece][pRotation][1];
+}
+
+
 //Pieces Definition
 int PiecesArray[7 /* kind */][4 /* rotation */][5 /* row */][5 /* column */] ={
 
