887786a35141fa1ae9330bd8933bfe77a26b03ea
anilberg <anilberg@users.noreply.github.com>
2020-09-15 20:00:46 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,74 +1,31 @@
 #include "Board.h"
 
-/*
-======================================
-Init the board blocks with free positions
-======================================
-*/
-void Board::InitBoard()
-{
-    for (int i = 0; i < BOARD_WIDTH; i++){
-        for (int j = 0; j < BOARD_HEIGHT; j++){
-             mBoard[i][j] = POS_FREE;
-        }
-    }
-}
-
-/*
-======================================
-Delete a line of the board by moving all above lines down
-
+/* 
+======================================                                  
+Returns the horizontal position (in pixels) of the block given like parameter
+ 
 Parameters:
-
->> pY:        Vertical position in blocks of the line to delete
-======================================
+ 
+>> pPos:  Horizontal position of the block in the board
+====================================== 
 */
-void Board::DeleteLine(int pY){
-
-    // Moves all the upper lines one row down
-    for(int j = pY; j > 0; j--){
-        for(int i = 0; BOARD_WIDTH; i++){
-            mBoard[i][j] = mBoard[i][j-1];
-        }
-    }
-}
-
-
-/* Parameters:
-
->> pX:        Horizontal position in blocks
->> pY:        Vertical position in blocks
->> pPiece:    Piece to draw
->> pRotation: 1 of the 4 possible rotations */
-
-void Board::StorePiece (int pX, int pY, int pPiece, int pRotation){                    //Store a piece in the board by filling the blocks
-    for (int i1 = pX, i2 = 0; i1 < pX + PIECE_BLOCKS; i1++, i2++)
-    {
-        for (int j1 = pY, j2 = 0; j1 < pY + PIECE_BLOCKS; j1++, j2++)
-        {
-            if (mPieces->GetBlockType (pPiece, pRotation, j2, i2) != 0)
-                mBoard[i1][j1] = POS_FILLED;
-        }
-    }
-}
-
-
-void Board::DeletePossibleLines ()                                                     // we should remove unnecessary lines
+int Board::GetXPosInPixels (int pPos)
 {
-    for (int i = 0; i < BOARD_HEIGHT; i++)
-    {
-        int j = 0;
-        while (j < BOARD_WIDTH)
-        {
-            if (mBoard[j][i] != POS_FILLED)
-            break;
-
-            i++;
-        }
-        if (i == BOARD_WIDTH)
-
-        DeleteLine (i);
-    }
+    return  ( ( BOARD_POSITION - (BLOCK_SIZE * (BOARD_WIDTH / 2)) ) + (pPos * BLOCK_SIZE) );
+}
+ 
+/* 
+======================================                                  
+Returns the vertical position (in pixels) of the block given like parameter
+ 
+Parameters:
+ 
+>> pPos:  Horizontal position of the block in the board
+====================================== 
+*/
+int Board::GetYPosInPixels (int pPos)
+{
+    return ( (mScreenHeight - (BLOCK_SIZE * BOARD_HEIGHT)) + (pPos * BLOCK_SIZE) );
 }
 
 /*
@@ -83,8 +40,11 @@ Parameters:
 */
 bool Board::IsFreeBlock (int pX,int pY)
 {
-     if (mBoard [pX][pY] == POS_FREE) return true; else return false;
-     }
+    if (mBoard [pX][pY] == POS_FREE) 
+        return true; 
+    else 
+        return false;
+}
 
 
    /*
@@ -128,7 +88,46 @@ bool Board::IsPossibleMovement (int pX,int pY, int pPiece, int pRotation){
     return true;
 }
 
-bool Board::IsGameOver(){                                                       //Checks If the first line has blocks. IF it has, than game is over
+/* Parameters:
+
+>> pX:        Horizontal position in blocks
+>> pY:        Vertical position in blocks
+>> pPiece:    Piece to draw
+>> pRotation: 1 of the 4 possible rotations */
+
+//Store a piece in the board by filling the blocks
+void Board::StorePiece (int pX, int pY, int pPiece, int pRotation){                    
+    for (int i1 = pX, i2 = 0; i1 < pX + PIECE_BLOCKS; i1++, i2++)
+    {
+        for (int j1 = pY, j2 = 0; j1 < pY + PIECE_BLOCKS; j1++, j2++)
+        {
+            if (mPieces->GetBlockType (pPiece, pRotation, j2, i2) != 0)
+                mBoard[i1][j1] = POS_FILLED;
+        }
+    }
+}
+
+// we should remove unnecessary lines
+void Board::DeletePossibleLines ()                                                     
+{
+    for (int i = 0; i < BOARD_HEIGHT; i++)
+    {
+        int j = 0;
+        while (j < BOARD_WIDTH)
+        {
+            if (mBoard[j][i] != POS_FILLED)
+            break;
+
+            i++;
+        }
+        if (i == BOARD_WIDTH)
+
+        DeleteLine (i);
+    }
+}
+
+//Checks If the first line has blocks. IF it has, than game is over
+bool Board::IsGameOver(){                                                       
     for (int i = 0; i < BOARD_WIDTH; i++)
     {
         if (mBoard[i][0] == POS_FILLED)
@@ -136,31 +135,41 @@ bool Board::IsGameOver(){                                                       
     }
     return false;
 }
-/* 
-======================================                                  
-Returns the horizontal position (in pixels) of the block given like parameter
- 
-Parameters:
- 
->> pPos:  Horizontal position of the block in the board
-====================================== 
+
+/*
+======================================
+Init the board blocks with free positions
+======================================
 */
-int Board::GetXPosInPixels (int pPos)
+void Board::InitBoard()
 {
-    return  ( ( BOARD_POSITION - (BLOCK_SIZE * (BOARD_WIDTH / 2)) ) + (pPos * BLOCK_SIZE) );
+    for (int i = 0; i < BOARD_WIDTH; i++){
+        for (int j = 0; j < BOARD_HEIGHT; j++){
+             mBoard[i][j] = POS_FREE;
+        }
+    }
 }
- 
- 
-/* 
-======================================                                  
-Returns the vertical position (in pixels) of the block given like parameter
- 
+
+/*
+======================================
+Delete a line of the board by moving all above lines down
+
 Parameters:
- 
->> pPos:  Horizontal position of the block in the board
-====================================== 
+
+>> pY:        Vertical position in blocks of the line to delete
+======================================
 */
-int Board::GetYPosInPixels (int pPos)
-{
-    return ( (mScreenHeight - (BLOCK_SIZE * BOARD_HEIGHT)) + (pPos * BLOCK_SIZE) );
+void Board::DeleteLine(int pY){
+
+    // Moves all the upper lines one row down
+    for(int j = pY; j > 0; j--){
+        for(int i = 0; BOARD_WIDTH; i++){
+            mBoard[i][j] = mBoard[i][j-1];
+        }
+    }
 }
+
+
+
+
+
