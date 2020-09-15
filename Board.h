4f644d243257985aec5213ec92e6353f64a479a5
anilberg <anilberg@users.noreply.github.com>
2020-09-15 18:39:04 +0300

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,7 +1,20 @@
 #ifndef _BOARD_
 #define _BOARD_
 
-
+// ------ Includes -----
+ 
+#include "Pieces.h"
+ 
+// ------ Defines -----
+ 
+#define BOARD_LINE_WIDTH 6          // Width of each of the two lines that delimit the board
+#define BLOCK_SIZE 16               // Width and Height of each block of a piece
+#define BOARD_POSITION 320          // Center position of the board from the left of the screen
+#define BOARD_WIDTH 10              // Board width in blocks 
+#define BOARD_HEIGHT 20             // Board height in blocks
+#define MIN_VERTICAL_MARGIN 20      // Minimum vertical margin for the board limit      
+#define MIN_HORIZONTAL_MARGIN 20    // Minimum horizontal margin for the board limit
+#define PIECE_BLOCKS 5              // Number of horizontal and vertical blocks of a matrix piece
 
 
 #endif
